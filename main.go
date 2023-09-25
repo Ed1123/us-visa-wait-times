9a9dd81cc4087e80f8e46fd3812c6c88d5b8f66f
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2023-09-24 22:41:35 -0500

    Add structs to store results
    Parse results as json

--- main.go
+++ main.go
@@ -1,25 +1,56 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
+type WaitTimeDetails struct {
+	days    int16
+	hasDays bool
+	message string
+}
+
+type CityWaitingTime struct {
+	cityName                string
+	studentExchangeVisitor  WaitTimeDetails
+	petitionBasedTempWorker WaitTimeDetails
+	crewTransit             WaitTimeDetails
+	businessTourismVisitor  WaitTimeDetails
+	// Country                 string
+}
+
+func parseWaitingTime(str string) WaitTimeDetails {
+	if v, ok := strconv.Atoi(strings.Split(str, " ")[0]); ok == nil {
+		return WaitTimeDetails{int16(v), true, ""}
+	} else {
+		return WaitTimeDetails{0, false, str}
+	}
+}
+
 func main() {
 	c := colly.NewCollector(colly.CacheDir("./us_visa_cache"))
+
+	cities := []CityWaitingTime{}
 
 	// Find and visit all links
 	c.OnHTML(
 		"div.tsg-rwd-body-frame-row div div.tsg-rwd-main-copy-frame.dataCSIpage div.tsg-rwd-main-copy-body-frame.no-rail.dataCSIpage div.tsg-rwd-content-page-parsysxxx.parsys div.tsg-rwd-text.parbase.section div p table tbody",
 		func(e *colly.HTMLElement) {
 			e.ForEach("tr", func(_ int, e *colly.HTMLElement) {
-				fmt.Println(e.ChildText("td:nth-child(1)"))
-				fmt.Println(e.ChildText("td:nth-child(2)"))
-				fmt.Println(e.ChildText("td:nth-child(3)"))
-				fmt.Println(e.ChildText("td:nth-child(4)"))
-				fmt.Println(e.ChildText("td:nth-child(5)"))
-				fmt.Println("end of row")
+				cityWaitTime := CityWaitingTime{
+					e.ChildText("td:nth-child(1)"),
+					parseWaitingTime(e.ChildText("td:nth-child(2)")),
+					parseWaitingTime(e.ChildText("td:nth-child(3)")),
+					parseWaitingTime(e.ChildText("td:nth-child(4)")),
+					parseWaitingTime(e.ChildText("td:nth-child(5)")),
+				}
+				cities = append(cities, cityWaitTime)
 			})
 		})
 
@@ -28,4 +59,10 @@ func main() {
 	})
 
 	c.Visit("https://travel.state.gov/content/travel/en/us-visas/visa-information-resources/global-visa-wait-times.html")
+
+	jsonBytes, err := json.Marshal(cities)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(jsonBytes))
 }
