db2cd0b79194e3c9ae4279d1834b3dfb9c56a8e8
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2023-09-27 17:48:50 -0500

    Refactor Visa waiting times scraper into own package

--- main.go
+++ main.go
@@ -1,83 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"strconv"
-	"strings"
 
+	"github.com/Ed1123/us-visa-waiting-times/usvisa"
 	"github.com/gin-gonic/gin"
-	"github.com/gocolly/colly"
 )
-
-// func getCountryForCity(cityName string) string {
-// 	return cityName
-// }
-
-type Days int16
-type Message string
-
-type WaitingTime struct {
-	Days    *Days    `json:"days,omitempty"`
-	Message *Message `json:"message,omitempty"`
-}
-
-type CityWaitingTime struct {
-	CityName string
-	// Country                 string
-	StudentExchangeVisitor  WaitingTime
-	PetitionBasedTempWorker WaitingTime
-	CrewTransit             WaitingTime
-	BusinessTourismVisitor  WaitingTime
-}
-
-func parseWaitingTime(str string) WaitingTime {
-	if v, ok := strconv.Atoi(strings.Split(str, " ")[0]); ok == nil {
-		days := Days(int16(v))
-		return WaitingTime{&days, nil}
-	} else if str != "" {
-		message := Message(str)
-		return WaitingTime{nil, &message}
-	}
-	return WaitingTime{nil, nil}
-}
-
-func GetWaitingData() []CityWaitingTime {
-	c := colly.NewCollector(colly.CacheDir("./us_visa_cache"))
-
-	cities := []CityWaitingTime{}
-
-	// Find and visit all links
-	c.OnHTML(
-		"p table tbody",
-		func(e *colly.HTMLElement) {
-			e.ForEach("tr:nth-child(n+2)", func(_ int, el *colly.HTMLElement) {
-				cityName := el.ChildText("td:nth-child(1)")
-				cityWaitTime := CityWaitingTime{
-					cityName,
-					// getCountryForCity(cityName),
-					parseWaitingTime(el.ChildText("td:nth-child(2)")),
-					parseWaitingTime(el.ChildText("td:nth-child(3)")),
-					parseWaitingTime(el.ChildText("td:nth-child(4)")),
-					parseWaitingTime(el.ChildText("td:nth-child(5)")),
-				}
-				cities = append(cities, cityWaitTime)
-			})
-		})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	c.Visit("https://travel.state.gov/content/travel/en/us-visas/visa-information-resources/global-visa-wait-times.html")
-
-	// jsonBytes, err := json.Marshal(cities)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(string(jsonBytes))
-	return cities
-}
 
 func main() {
 	router := gin.Default()
@@ -87,7 +15,7 @@ func main() {
 
 	router.LoadHTMLFiles("./templates/table.tmpl")
 
-	cities := GetWaitingData()
+	cities := usvisa.GetWaitingData()
 
 	// Define a route to display the table
 	router.GET("/", func(c *gin.Context) {
