2c0f44f8494641cc63ba84b3bd9e20980067c270
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2023-10-21 19:44:04 -0500

    Add CityInfoCache
    caches api calls to a local file
    ignore cache file

--- city-api/city_api.go
+++ city-api/city_api.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 )
 
 type response struct {
@@ -48,7 +49,12 @@ func GetCityInfo(city string) CityInfo {
 	q.Add("q", city)
 	// q.Add("no_annotations", "1") // uncomment to include flag
 	q.Add("limit", "1")
-	q.Add("key", os.Getenv("OPENCAGE_API_KEY"))
+
+	apiKey, isApiKey := os.LookupEnv("OPENCAGE_API_KEY")
+	if !isApiKey {
+		log.Fatalln("Couldn't find OPENCAGE_API_KEY in environment variables")
+	}
+	q.Add("key", apiKey)
 
 	u.RawQuery = q.Encode()
 
@@ -65,11 +71,84 @@ func GetCityInfo(city string) CityInfo {
 	if err := json.Unmarshal(body, &r); err != nil {
 		log.Fatalln("Couldn't unmarshal response", err)
 	}
-
+	if len(r.Features) == 0 {
+		log.Println("No city info found for", city)
+		return CityInfo{CityName: city}
+	}
 	return CityInfo{
 		CityName:  city,
 		Country:   r.Features[0].Properties.Components.Country,
 		Latitude:  r.Features[0].Geometry.Coordinates[0],
 		Longitude: r.Features[0].Geometry.Coordinates[1],
+	}
+}
+
+type CityInfoCache struct {
+	cache map[string]CityInfo
+	file  string
+}
+
+func NewCityInfoCache() CityInfoCache {
+	cityCache := CityInfoCache{
+		cache: make(map[string]CityInfo),
+		file:  "./.cache/city_info_cache.json",
+	}
+	cityCache.readFromDisk()
+	return cityCache
+}
+
+func (c CityInfoCache) getCityInfo(city string) (CityInfo, bool) {
+	ci, ok := c.cache[city]
+	return ci, ok
+}
+
+func (c CityInfoCache) setCityInfo(city string, ci CityInfo) {
+	c.cache[city] = ci
+	c.writeToDisk()
+}
+
+func (c CityInfoCache) readFromDisk() error {
+	f, err := os.Open(c.file)
+	if os.IsNotExist(err) {
+		dir := filepath.Dir(c.file)
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			log.Fatalln("Couldn't create cache directory", err)
+		}
+		c.writeToDisk()
+		return nil
+	}
+	if err != nil {
+		log.Fatalln("Couldn't open cache file.", err)
+	}
+	defer f.Close()
+
+	err = json.NewDecoder(f).Decode(&c.cache)
+	if err != nil {
+		log.Fatalln("Couldn't decode cache file", err)
+	}
+	return nil
+}
+
+func (c CityInfoCache) writeToDisk() {
+	f, err := os.Create(c.file)
+	if err != nil {
+		log.Fatalln("Couldn't open cache file", err)
+	}
+	err = json.NewEncoder(f).Encode(c.cache)
+	if err != nil {
+		log.Fatalln("Couldn't encode cache file", err)
+	}
+}
+func (c CityInfoCache) GetCityInfo(city string) CityInfo {
+	cityInfo, inCache := c.getCityInfo(city)
+	if inCache {
+		log.Println("Getting city info from cache.")
+		return cityInfo
+	} else {
+		log.Println("Getting city info from API for", city, "and caching it.")
+		cityInfo := GetCityInfo(city)
+		c.setCityInfo(city, cityInfo)
+		return cityInfo
 	}
 }
